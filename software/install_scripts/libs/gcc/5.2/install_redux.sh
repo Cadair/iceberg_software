182c36663da3f33dc96f576b0c9a5a97f27990fe
Nabil Freij <nabil.freij@gmail.com>
2016-05-30 18:18:58 +0200

    Update install_redux.sh

--- software/install_scripts/libs/gcc/5.2/install_redux.sh
+++ software/install_scripts/libs/gcc/5.2/install_redux.sh
@@ -7,29 +7,27 @@
 # This script will then create the directories you need and download and unzip
 # the source in to the build dir.
 
-
 ############################# Module Loads ###################################
 module load compilers/gcc/5.2
 module load apps/idl/8.5
+module load libs/gcc/4.8.2/boost/1.58
 module load libs/gcc/5.2/fftw/3.3.4
-module load libs/gcc/5.2/boost/1.59
+module load mpi/gcc/openmpi
 
 ############################## Variable Setup ################################
 name=redux
-version=2016.05.16
+version=2016.05.30
 prefix=/usr/local/packages6/libs/gcc/5.2/$name/$version
 build_dir=/scratch/$USER/$name
 
 # Set this to 'sudo' if you want to create the install dir using sudo.
 sudo=''
 
-
 ##############################################################################
 # This should not need modifying
 ##############################################################################
 
 # Create the build dir
-
 if [ ! -d $build_dir ]
 then
     mkdir -p $build_dir
@@ -45,7 +43,7 @@ then
 fi 
 
 # Download the source
-GIT_DIR=redux
+GIT_DIR=$name
 if [ -d $GIT_DIR ]
 then                                                                            
     cd $GIT_DIR
@@ -58,16 +56,16 @@ else
 fi
 
 ##############################################################################
-
-##############################################################################
 # Installation (Write the install script here)
 ##############################################################################
+
 ARCH=$(uname -m)
-BOOST=/usr/local/packages6/libs/gcc/4.8.2/boost/1.58.0
+export BOOST_DIR=/usr/local/packages6/libs/gcc/4.8.2/boost/1.58.0
+FFTW3=/usr/local/packages6/libs/gcc/5.2/fftw/3.3.4
 
 mkdir build
 cd build
-cmake ../ -DIDL_DLM_DIR:STRING=$prefix/dlm/$ARCH -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_LIBRARY_PATH:STRING=$BOOST/lib -DCMAKE_INCLUDE_PATH:STRING=$BOOST/include
+cmake ../ -DIDL_DLM_DIR:STRING=$prefix/dlm/$ARCH -DCMAKE_INSTALL_PREFIX:STRING=$prefix -DCMAKE_LIBRARY_PATH:STRING=$FFTW3/lib -DCMAKE_INCLUDE_PATH:STRING=$FFTW3/include
 
 make GIT_CHECK_LIBREDUX
 cd src/dlm/
